3d25127e6ae2c6029f9d714e2d1449a675dc4240
Yasmine Amami <yasmine.amami@polymtl.ca>
2025-02-01 18:15:25 -0500

    Code final

--- TrashDetection.cpp
+++ TrashDetection.cpp
@@ -1,3 +1,4 @@
+//#include "powerMotorPin"
 #include "SR04.h"
 
 long distance;
@@ -16,7 +17,7 @@ const int vccTemp = 11;
 SR04 capteurUltraSon = SR04(trigPin, echoPin);
 
 void setup() {
-    pinMode(3, OUTPUT);
+    pinMode(6, OUTPUT);
     pinMode(11, OUTPUT);
     pinMode(motor1pin1, OUTPUT);
     pinMode(motor1pin2, OUTPUT);
@@ -28,12 +29,29 @@ void setup() {
 void loop() {
     digitalWrite(11, 255);
     distance = capteurUltraSon.Distance();
-    if (distance <= 25) digitalWrite(12, 1);
-    else digitalWrite(12, 0);
+    if (distance <= 15) {
+      digitalWrite(6, 1);
+      digitalWrite(2, 0);
+      digitalWrite(3, 0);
+      digitalWrite(4, 0);
+      digitalWrite(5, 0);
+
+
+
+    }
+    else {
+      digitalWrite(6, 0);
+      digitalWrite(2, 1);
+      digitalWrite(3, 1);
+      digitalWrite(4, 1);
+      digitalWrite(5, 1);
+
+    } 
 
     digitalWrite(motor1pin1, HIGH);
     digitalWrite(motor1pin2, LOW);  // Le moteur tourne dans un sens
 
     digitalWrite(motor2pin1, LOW);
     digitalWrite(motor2pin2, HIGH);  // Le moteur tourne dans un sens
+    //delay(3000);
 }
